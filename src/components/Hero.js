ddc8449ef7c643d95a71f073d6ed949f6fcacdaf
Sarthak412 <sarthakkamble412@gmail.com>
2023-05-08 16:05:25 -0700

    Final commits

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
   }, [])
 
   return (
-    <div id='#hero' className='h-[650px] w-full flex items-center justify-center px-5 py-8 bg-gradient-to-t from-blue-950 to-indigo-300'>
+    <div id='#hero' className='h-[650px] w-full overflow-hidden flex items-center justify-center px-5 py-8 bg-gradient-to-t from-blue-950 to-indigo-300'>
         <div data-aos="zoom-in" className='mx-auto px-5 py-8'>
             <h3 className='text-[45px] text-white text-start text-shadow'>Hola,</h3>
             <h1 className='text-[55px] text-start text-shadow-white'>Welcome to WebMagic</h1>
